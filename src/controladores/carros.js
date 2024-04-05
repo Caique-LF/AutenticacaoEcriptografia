282e9aacb3daa9ea08a88a5254997ed41093b86f
Caique L. Fernandes <caique.l.f9.clf@gmail.com>
2024-04-05 18:45:21 -0400

    Implementando cadastro de carros

--- src/controladores/carros.js
+++ src/controladores/carros.js
@@ -13,7 +13,7 @@ const listarCarros = async (req, res)=>{
 
 
 const detalharCarro = async (req, res)=>{
-    const {id} = req.body
+    const {id} = req.params
 
     try {
         const {rows, rowCount} = await pool.query('select * from carros where id = $1', [id])
@@ -30,9 +30,23 @@ const detalharCarro = async (req, res)=>{
 };
 
 const cadastrarCarros = async (req, res)=>{
+    const {modelo, marca, ano, cor, descricao} = req.body
 
+    try {
+        const{rows} = await pool.query('insert into carros (modelo, marca, ano, cor, descricao) values ($1, $2, $3, $4, $5) returning *',
+        [modelo, marca, ano, cor, descricao])
+
+        return res.status(201).json(rows[0])
+
+    } catch (error) {
+        return res.status(500).json({menssagem: "Erro interno no servidor"})
+    }
 };
 
+
+
 module.exports = {
-    listarCarros
+    listarCarros,
+    detalharCarro,
+    cadastrarCarros
 }
